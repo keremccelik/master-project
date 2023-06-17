4818a866a07756d59e3e3bdc77ac00e2bd98fdc1
keremccelik <keremccelik@gmail.com>
2023-06-17 20:02:41 +0300

    second try

--- app/page.js
+++ app/page.js
@@ -2,5 +2,10 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}>Hello world</main>;
+  return (
+    <main className={styles.main}>
+      <p>Hello world</p>
+      <button className={styles.button}>Click me</button>
+    </main>
+  );
 }
