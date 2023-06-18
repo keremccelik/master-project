12cf3ecda55f63bb9e06d775287644d83df82c6a
keremccelik <keremccelik@gmail.com>
2023-06-18 13:53:41 +0300

    sanity imageurl resolved

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
 import styles from "./page.module.css";
 import Link from "next/link";
 import Footer from "@/components/Footer/Footer";
-import useClient from "@/hooks/useClient";
+import useClient from "@/lib/useClient";
+import Image from "next/image";
+import urlFor from "@/lib/urlFor";
 
 export default async function Home() {
   const client = useClient();
@@ -12,6 +14,16 @@ export default async function Home() {
     <main className={styles.main}>
       <p>Homepage</p>
       <Link href="/about">go to About Page</Link>
+      {properties.map((property) => (
+        <div key={property._id}>
+          <h2>{property.title}</h2>
+          <Image
+            src={urlFor(property.mainImage).url()}
+            width={200}
+            height={200}
+          />
+        </div>
+      ))}
       <Footer />
     </main>
   );
