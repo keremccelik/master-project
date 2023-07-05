6fc4e610927be53679f6a2fb140746d5476dc8f3
keremccelik <keremccelik@gmail.com>
2023-07-05 08:40:15 +0300

    minor change

--- app/(project)/components/Whatsapp/Whatsapp.js
+++ app/(project)/components/Whatsapp/Whatsapp.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Whatsapp() {
   return (
     <div className={styles.iconWrapper}>
-      <Link href="https://wa.me/05533382384">
+      <Link href="https://wa.me/905533382384">
         <Image
           src="/assets/wp-logo.png"
           alt="whatsapp"
