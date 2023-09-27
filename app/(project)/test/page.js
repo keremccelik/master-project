93383b837f95dc33f263c2af0ef23cb2a31328c6
keremccelik <keremccelik@gmail.com>
2023-09-27 15:34:23 +0300

    language support

--- app/(project)/test/page.js
+++ app/(project)/test/page.js
@@ -1,20 +1,15 @@
+"use client";
 import styles from "./page.module.css";
 import Container from "../components/Commons/Container/Container";
 import Grid from "../components/Commons/Grid/Grid";
+import React, { useContext, useEffect } from "react";
 
 export default function Test() {
+
   return (
     <main>
-      <Container>
-        <Grid>
-          <div className={styles.module1}>SLİDE</div>
-          <div className={styles.module2}>FİYAT</div>
-          <div className={styles.module3}>AGENT</div>
-          <div className={styles.module4}>SPECS</div>
-          <div className={styles.module5}>HARİTA</div>
-          <div className={styles.module6}>AÇIKLAMA</div>
-        </Grid>
-      </Container>
+
+      <p>Hello World</p>
     </main>
   );
 }
---
+++ app/(project)/utils/constants.js
@@ -0,0 +1,6 @@
+export const links = [
+  { title: "Home", path: "/" },
+  { title: "Properties", path: "/properties" },
+  { title: "About us", path: "/about-us" },
+  { title: "Contact", path: "/contact" },
+];
